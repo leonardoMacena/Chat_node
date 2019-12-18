b902ec5e5eea56b1a9360485f43d6d9cf1d3599c
leonardo Macena <leonardofernando6@gmail.com>
2019-12-18 10:45:52 -0300

    creted scirpt in page

--- src/server.js
+++ src/server.js
@@ -13,4 +13,20 @@ app.set('views', path.join(__dirname, 'public'))
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
-app.use('/')
+app.use('/', (req, res) => {
+  res.render('index.html')
+})
+
+let messages = []
+
+io.on('connection', socket => {
+
+  socket.emit('previousMessages', messages)
+
+  socket.on('sendMessage', data => {
+    messages.push(data)
+    socket.broadcast.emit('receivedMessage', data)
+  })
+})
+
+server.listen(3000)
